34279eb82e72611f1cb1962a692884d5333d5fab
sane <374023109.com>
2024-06-22 09:53:40 +0800

    修复周末条件

--- internal/sign/sign.go
+++ internal/sign/sign.go
@@ -31,7 +31,7 @@ func init() {
 	if apiURL = os.Getenv("API_URL"); apiURL == "" {
 		apiURL = "https://api.day.app/"
 	}
-
+	
 	CmdSign.Flags().StringVarP(&apiURL, "api-url", "r", apiURL, "bark server api")
 	CmdSign.Flags().StringVarP(&body, "body", "b", body, "推送加密消息内容")
 	CmdSign.Flags().StringVarP(&key, "key", "k", key, "推送加密 key")
@@ -42,24 +42,24 @@ func run(_ *cobra.Command, args []string) {
 	if len(args) == 0 {
 		panic("参数不足: 请输入推送设备标识")
 	}
-
+	
 	now, deviceKey := time.Now(), args[0]
 	dayOfWeek, nowFormat := now.Weekday(), now.Format(dateFormat)
-
+	
 	dayData, _ := getDayData(now)
 	weekName := weekdays[dayOfWeek]
-
+	
 	if dayData != nil && !dayData.IsOffDay {
 		fmt.Printf("今天是: %s %s 为 %s, 签到结果: %v\n", nowFormat, weekName, dayData.Name, notify(deviceKey, nowFormat, weekName, dayData.Name))
 		return
 	}
-
+	
 	// 周末 或 节假日
-	if (dayData != nil && dayData.IsOffDay) && dayOfWeek == time.Saturday || dayOfWeek == time.Sunday {
+	if (dayData != nil && dayData.IsOffDay) || dayOfWeek == time.Saturday || dayOfWeek == time.Sunday {
 		fmt.Printf("今天是: %s %s, 休息啦\n", nowFormat, weekName)
 		return
 	}
-
+	
 	fmt.Printf("今天是: %s %s 为 %s, 签到结果: %v\n", nowFormat, weekName, "工作日", notify(deviceKey, nowFormat, weekName, "工作日"))
 }
 
@@ -69,6 +69,6 @@ func notify(deviceKey, date, weekName, dayName string) error {
 	if key != "" && iv != "" {
 		return util.SendToBark(apiURL, deviceKey, body, key, iv)
 	}
-
+	
 	return util.SendToBarkDirectly(apiURL, deviceKey, body)
 }
